ad39d5c9331e8c05bd368e3faa00e08e2139d586
saichandra-1 <saichandra0708@gmail.com>
2025-09-24 21:48:46 +0530

    changed the logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,12 +13,14 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "portfolio",
+  title: "Portfolio",
   description: "portfolio website for sai chandra",
   icons: {
-    icon: "/image.png",
-    shortcut: "/image.png",
-    apple: "/image.png",
+    icon: [
+      { url: "/image.png?v=3" } 
+       ],
+    shortcut: "/image.png?v=3",
+    apple: "/image.png?v=3",
   },
 };
 
@@ -31,7 +33,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
-
+        <link rel="icon" href="/image.png?v=3" sizes="any" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
